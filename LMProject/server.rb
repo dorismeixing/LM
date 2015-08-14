e74866c23c7c992e1e6ba2f54b6d12fc1f8f47c3
dorismeixing <lilylily3735@hotmail.com>
2015-08-14 16:42:44 -0400

    Create a new file called杂七杂八
    New file is to store some other documents that is less relevant to LM
    project.

--- LMProject/server.rb
+++ LMProject/server.rb
@@ -3,7 +3,6 @@ require "sinatra"
 
 get "/tasks" do
   ##tasks = ["pay bills", "buy milk", "learn Ruby", "HAHAHAHA"]
-
   erb :index ##, locals: { tasks: tasks }
 end
 
--- SinatraPractics/server.rb
+++ SinatraPractics/server.rb
@@ -1,12 +1,12 @@
 require "sinatra"
+#require "pry"
 
 get "/" do
   "Hello, world!"
 end
 
 get "/tasks" do
-  tasks = ["pay bills", "buy milk", "learn Ruby", "HAHAHAHA"]
-
+  tasks = File.readlines("tasks.txt")
   erb :index, locals: { tasks: tasks }
 end
 
@@ -17,6 +17,24 @@ end
 
 get "/hello" do
   "<p>Hello, world! The current time is #{Time.now}.</p>"
+end
+
+post "/tasks" do
+
+  # Temporarily insert a debugger breakpoint here so
+  # we can view the *params* hash.
+  #binding.pry
+  # Read the input from the form the user filled out
+  task = params["task_name"]
+
+  # Open the "tasks.txt" file and append the task
+  File.open("tasks.txt", "a") do |file|
+    file.puts(task)
+  end
+
+  # Send the user back to the home page which shows
+  # the list of tasks
+  redirect "/tasks"
 end
 
 # These lines can be removed since they are using the default values. They've
