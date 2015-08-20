aa06c7a83ec335bcf22b30bcfd5150c630f308d2
LinghanX <linghan@me.com>
2015-08-20 10:12:36 -0400

    2
    111

--- LMSinatra/server.rb
+++ LMSinatra/server.rb
@@ -5,15 +5,6 @@ get "/tasks" do
   erb :index ##, locals: { tasks: tasks }
 end
 
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 get "/hello" do
   "<p>Hello, world! The current time is #{Time.now}.</p>"
 end
-
-# These lines can be removed since they are using the default values. They've
-# been included to explicitly show the configuration options.
-set :views, File.join(File.dirname(__FILE__), "views")
-set :public_folder, File.join(File.dirname(__FILE__), "public")
